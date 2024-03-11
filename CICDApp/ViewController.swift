0ffbdecb1296de9bc2e082ee3a78299b9c067745
nilu.walmik@tissatech.com <nilu.walmik@tissatech.com>
2024-03-11 12:37:30 +0530

    first commit

---
+++ CICDApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CICDApp
+//
+//  Created by Tissa Macbook on 11/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("My new CICD App")
+    }
+
+
+}
+
